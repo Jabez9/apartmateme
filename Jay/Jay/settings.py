13dc122005efcaf04732486fe20dd2f363285ea9
Jabez9 <mukoshijabez@gmail.com>
2025-01-11 13:43:39 +0300

    changes to settings

--- Jay/Jay/settings.py
+++ Jay/Jay/settings.py
@@ -228,8 +228,8 @@ ADMIN_LOGIN_REDIRECT_URL = '/admin/'  # Redirect for default admin
 
 #Handling emails 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER =env('USER') 
-EMAIL_HOST_PASSWORD =env('PASSWORD') 
+EMAIL_HOST_USER =env('EMAIL_HOST_USER') 
+EMAIL_HOST_PASSWORD =env('EMAIL_HOST_PASSWORD') 
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
